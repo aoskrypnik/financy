49662bfcfb8e379f60bc00dd535ce7ab11f4a0cf
andrew <andrii.skrypnik@gmail.com>
2020-04-26 01:31:18 +0300

    Edited node dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.70"
     id("org.jetbrains.kotlin.plugin.noarg") version "1.3.70"
-    id("com.moowork.node") version "1.3.1"
+    id("com.github.node-gradle.node") version "1.5.3"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 }
